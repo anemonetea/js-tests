2a34778a89c854d9cc630ff4d9589083930349f6
Anastasiya Lazarenko <anastasiya.lazarenko@ibm.com>
2024-06-07 15:00:29 -0400

    Multiple named exports at once in const4

---
+++ module-tests/constants4.js
@@ -0,0 +1,5 @@
+const STR1 = "Hello"
+const STR2 = " World"
+const STR3 = "!"
+
+export { STR1, STR2, STR3 };
--- module-tests/main.js
+++ module-tests/main.js
@@ -10,14 +10,16 @@ import * from './constants3.js';  // there is `export * from "mod"` but there is
 */
 
 import * as constants3 from './constants3.js';
+import * as CONST4 from './constants4.js';
 
 
 export default function main() {
     console.log("0: Hello world!");
     console.log("1:", STR1());
-    console.log("2:", STR1_1, STR2, STR3);
-    console.log("3:", str());
-    console.log("4:", constants3.STR1NG, constants3.STRING2, constants3.namedFunction());
+    console.log("2.1:", STR1_1, STR2, STR3);
+    console.log("2.2:", str());
+    console.log("3:", constants3.STR1NG, constants3.STRING2, constants3.namedFunction());
+    console.log("4:", CONST4.STR1 + CONST4.STR2 + CONST4.STR3);
     let world1 = new MyWorld();
     console.log("world1:", world1);
     console.log("world1 hello:", world1.getHello());
